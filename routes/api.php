ef68e85c0f52ada6609066533cbd710c6e2ebb95
Devsolution <q3construction1@gmail.com>
2020-07-20 16:20:26 +0800

    Update route

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/token', 'API\AuthController@getToken');
+Route::post('/register', 'API\AuthController@register');
