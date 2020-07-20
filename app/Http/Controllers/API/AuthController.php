c0445f7fa7e025ba732c88684290b5ddf22ce941
Devsolution <q3construction1@gmail.com>
2020-07-20 23:29:59 +0800

    Register, show, update profile

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -44,7 +44,7 @@ class AuthController extends Controller
 
         return response()->json([
             'success' => true,
-            'token' => $user->createToken($request->device_name)->plainTextToken,
+            'token' => $user->createToken($request->device_name, ['server:update'])->plainTextToken,
             'user' => [
                 'name' => $user->name,
                 'email' => $user->email,
@@ -60,32 +60,112 @@ class AuthController extends Controller
      */
     public function register(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'email' => 'required|email|unique:users',
-            'password' => 'required',
-            'name' => 'required',
-        ]);
-        if ($validator->fails()) {
-            $error_messages = $validator->errors()->messages();
-            $error_keys = array_keys($error_messages);
-            foreach ($error_keys as $error_key) {
-                $error_result[$error_key] = $error_messages[$error_key];
+        try {
+            $validator = Validator::make($request->all(), [
+                'email' => 'required|email|unique:users',
+                'password' => 'required',
+                'name' => 'required',
+            ]);
+    
+            if ($validator->fails()) {
+                $error_messages = $validator->errors()->messages();
+                $error_keys = array_keys($error_messages);
+                foreach ($error_keys as $error_key) {
+                    $error_result[$error_key] = $error_messages[$error_key];
+                }
+                return response()->json([
+                    'success' => false,
+                    'error' => $error_result
+                ]);
             }
+    
+            User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password)
+            ]);
+    
+            return response()->json([
+                'success' => true,
+                'message' => 'The user is registered successfully.'
+            ]);
+        } catch (Exception $error) {
             return response()->json([
                 'success' => false,
-                'error' => $error_result
+                'error' => $error->getMessage()
             ]);
         }
+    }
+    
+    /**
+     * Profile update
+     * @param $request
+     * @param $user
+     * @return $jsonResponse
+     * @route('/api/profile/{user}', method='PUT')
+     */
+    public function updateProfile(User $user, Request $request)
+    {
+        try {
+            if ($user->id != $request->user()->id) {
+                return response()->json([
+                    'success' => false,
+                    'error' => "You can't update profile of another user."
+                ]);
+            }
+            $validator = Validator::make($request->all(), [
+                'email' => 'required|email|unique:users,email,' . $user->id,
+                'password' => 'required',
+                'name' => 'required',
+            ]);
+    
+            if ($validator->fails()) {
+                $error_messages = $validator->errors()->messages();
+                $error_keys = array_keys($error_messages);
+                foreach ($error_keys as $error_key) {
+                    $error_result[$error_key] = $error_messages[$error_key];
+                }
+                return response()->json([
+                    'success' => false,
+                    'error' => $error_result
+                ]);
+            }
+    
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = Hash::make($request->password);
+            $user->save();
+    
+            return response()->json([
+                'success' => true,
+                'message' => 'The profile is updated successfully.'
+            ]);
+        } catch (Exception $error) {
+            return response()->json([
+                'success' => false,
+                'error' => $error->getMessage()
+            ]);
+        }
+    }
 
-        User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password)
-        ]);
-
+    /**
+     * Profile show
+     * @param $request
+     * @param $user
+     * @return $jsonResponse
+     * @route('/api/profile/{user}', method='GET')
+     */
+    public function showProfile(User $user, Request $request)
+    {
         return response()->json([
             'success' => true,
-            'message' => 'The user is registered successfuly.'
+            'data' => [
+                'id' => $user->id,
+                'name' => $user->name,
+                'email' => $user->email,
+                'created_at' => $user->created_at,
+                'updated_at' => $user->updated_at,
+            ]
         ]);
     }
 }
